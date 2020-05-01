9f6cea4d00d28dced2978a696b5644b134b5eb17
Leroy Grant <lgrant146@gmail.com>
2020-05-01 09:02:14 -0500

    while edit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,31 +1,59 @@
-"use strict";
+(function() {
 
-// var skippingNumber;
-//
-// while (typeof skippingNumber !== "number") {
-//    skippingNumber = parseFloat(prompt("what is then number you are skipping?"));
-//     }
+    "use strict";
 
-// for (var i = 1; i <= 50; i++){
-//     if (i % 2 !== 0){
-//         console.log("Here is an odd number:" + i);
+// Prompt and check if number is odd between 1 and 50 and also a number
 
-
-function oddNumber(input){
-    if(input % 2 !== 0){
-        console.log( "skipping the number: " + numberInput);
+    function isInRange(input, min, max) {
+        return input >= min && input <= max;
     }
-}
-while (true) {
-    var numberInput = prompt("Enter an odd number between 1 and 50");
 
-    if (numberInput <= 50 && numberInput >= 1 && !isNaN(numberInput)) {
-        oddNumber();
-        break;
-    } else if (numberInput === '') {
-        alert('that is an invalid number') {
-            break;
+    function isNumber(input) {
+        return !isNaN(input);
+    }
+
+    function isInteger(input) {
+        return input === Math.floor(input);
+    }
+
+    function isOdd(input) {
+        return input % 2 !== 0;
+    }
+
+    function isValidInput(input) {
+        input = parseFloat(input);
+        return isInRange(input, 1, 50) && isNumber(input) && isOdd(input) && isInteger(input);
+    }
+
+    function getUserInput() {
+        var userInput;
+        while (true) {
+            userInput = prompt("Please enter an odd number between 1 and 50");
+            if (isValidInput(userInput)) {
+                userInput = parseInt(userInput);
+                break;
+            } else {
+                alert("Invalid input!");
+            }
+
         }
-    } else if (numberInput)
-}
+        return userInput;
+    }
 
+//put in a loop
+
+    function runNumbers(min, max) {
+        var userInput = getUserInput();
+        var output = "Number to skip is: " + userInput + '\n\n';
+        for (var i = min; i <= max; i += 1) {
+            if (!isOdd(i)) {
+                continue;
+            }
+            output += (userInput !== i) ? "Here is an odd number: " + i : "Yikes Skipping number: " + userInput;
+            output += "\n";
+        }
+        return output;
+    }
+
+    console.log(runNumbers(1, 50));
+})();
--- js/while.js
+++ js/while.js
@@ -1,12 +1,28 @@
-"use strict";
-function displayDoubleTwo(){
-    var output = "";
-    var i = 2;
-    while (i <= 65536) {
-        output +=i + "\n";
-        i *=2;
-    }
-    return output;
-}
+(function() {
+    "use strict";
 
-// console.log(displayDoubleTwo());
+    function displayDoubleTwo() {
+        var output = "";
+        var i = 2;
+        while (i <= 65536) {
+            output += i + "\n";
+            i *= 2;
+        }
+        return output;
+    }
+
+console.log(displayDoubleTwo());
+function addSorNot(num) {
+    return (num === 1) ? "" : "s";
+}
+function createTransactionMsg(conesSold, orderedCones, inventoryCones)
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    var requestedCones = Math.floor(Math.random() * 5) + 1;
+
+
+    do {
+        console.log("We have this many cones today: "  + allCones);
+
+
+    } while()
+})();
