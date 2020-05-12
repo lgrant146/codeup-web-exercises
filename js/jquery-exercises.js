26b8f3b1ebc4b31a629d36cccc707302eaed8771
Leroy Grant <lgrant146@gmail.com>
2020-05-12 12:32:42 -0500

    steps 1 2 3 of selectors exercise

---
+++ js/jquery-exercises.js
@@ -0,0 +1,13 @@
+
+
+(function ($){
+
+    "use strict";
+    $(document).ready(function(){
+        alert('DOM has finished loading!')
+    });
+
+    var mainHeader = $('#heading1').html();
+
+    alert(mainHeader);
+})(jQuery);
