0447fb1f26847d5bb3608de5157cd703236f569b
Leroy Grant <lgrant146@gmail.com>
2020-05-12 13:48:44 -0500

    selectors complete

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,7 +1,10 @@
 
 
 (function ($){
-
     "use strict";
-      $('.codeup').css('border', '1px solid red')
+
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background-color', '#FFFF00');
+    alert($('h1').html());
+
 })(jQuery);
