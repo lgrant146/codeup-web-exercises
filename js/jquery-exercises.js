cbca147762f8c75fb3f7c863c3e367686404b146
Leroy Grant <lgrant146@gmail.com>
2020-05-13 11:00:10 -0500

    finished jquery mouse events

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -8,5 +8,15 @@
     // alert($('h1').html());
     $('h1').click(function(){
         $(this).css('background-color', 'blue');
-    })
+    });
+    $('p').dblclick(function(){
+        $(this).css('font-size', '18px');
+    });
+    $('li').hover(function() {
+        $(this).css('color', 'red');
+    },
+        function () {
+        $(this).css('color', 'black');
+
+    });
 })(jQuery);
