b8878ca052e163a59ae039ad7b39746552b1de74
Leroy Grant <lgrant146@gmail.com>
2020-05-13 10:54:27 -0500

    blue background click

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -3,8 +3,10 @@
 (function ($){
     "use strict";
 
-    $('li').css('font-size', '20px');
-    $('h1, p, li').css('background-color', '#FFFF00');
-    alert($('h1').html());
-
+    // $('li').css('font-size', '20px');
+    // $('h1, p, li').css('background-color', '#FFFF00');
+    // alert($('h1').html());
+    $('h1').click(function(){
+        $(this).css('background-color', 'blue');
+    })
 })(jQuery);
