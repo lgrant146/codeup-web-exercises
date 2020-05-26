ec208d99d985dfd0e6d52ac3c71d9dc8ede8c835
Leroy Grant <lgrant146@gmail.com>
2020-05-26 10:39:21 -0500

    got last commit to work

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,12 @@
-let gitHubPromise = fetch( "https://api.github.com/users/lgrant146/events");
+// let gitHubPromise = fetch( "https://api.github.com/users/lgrant146/events");
 
+
+
+function lastCommit (userName){
+  let gitHubPromise = fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GITHUB_REPO}})
 gitHubPromise.then(response => response.json())
-    .then(jsonResponse => (console.log(jsonResponse)));
+    .then(jsonResponse => console.log((jsonResponse[0].created_at).substr(0,10)));
+}
+
+
+lastCommit(`lgrant146`);
